ac9a0d28495a68bb3277d6983526153508d7598c
yadonoya <mscottschreiber@gmail.com>
2022-05-18 19:42:13 +0900

    feat: add .env.local file and update knexfiles.js

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+// 
+
+module.exports = {
+	client     : 'pg',
+	connection : {
+		host     : process.env.DB_HOST,
+		port     : process.env.DB_PORT,
+		database : process.env.DB_NAME,
+		user     : process.env.DB_USER,
+		password : process.env.DB_PASSWORD,
+		charset: 'utf8'
+	},
+	migrations : {
+		directory : './db/migrations',
+		tableName : 'knex_migrations'
+	},
+	seeds      : {
+		directory : './db/seeds'
+	}
+};
