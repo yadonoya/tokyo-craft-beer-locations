04ba8a949aa42c6c9263b158e8ef99c17361fbd2
yadonoya <mscottschreiber@gmail.com>
2022-05-21 10:53:18 +0900

    feat: add cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const barsController = require('../controller/barsController');
+const cors = require('cors');
 
 
 const setupServer = () => {
   const app = express();
   app.use(express.json());
+
+  app.use(
+    cors({
+      origin : '*',
+          methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+          credentials: true
+    })
+  );
 
   app.use('/api/bars', barsController);
 
